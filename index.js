e5b65de42abe2e58bbf380966f3900f816cb50f6
amitkr492 <amtkr492@gmail.com>
2017-11-29 12:13:43 +0530

    first commit
    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get("/",function(req,res){
-	res.send("WElcome to nodejs on heroku!");
+	res.send("Hi, Welcome to nodejs on heroku!");
 });
 
 app.listen(port);
