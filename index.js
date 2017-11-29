42e043fe394bc1cca691426130207c2ff4f72f0e
amitkr492 <amtkr492@gmail.com>
2017-11-29 12:31:47 +0530

    first commmit
    Name change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get("/",function(req,res){
-	res.send("Hi, Welcome to nodejs on heroku!");
+	res.send("Hi, Vishal!");
 });
 
 app.listen(port);
